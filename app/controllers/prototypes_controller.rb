c5067bc54b5b4d272defd7283eadbcfbecd9b516
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-10-25 20:02:19 +0900

    画面遷移の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
 
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @prototype = Prototype.new
@@ -55,7 +54,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to root_path
     end
   end
 
