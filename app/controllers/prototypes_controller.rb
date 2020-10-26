c09bd140659781c91277145b6009b1c663688efc
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-10-26 22:56:51 +0900

    削除機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -42,7 +42,7 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
-     render :index
+     redirect_to root_path
   end
 
 
